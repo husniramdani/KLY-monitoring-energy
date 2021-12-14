39abfc01fbf75ac7ed2c6f0a6c0503e7c67ad7ee
husniramdani <husni.ramdani@codemi.co.id>
2021-12-15 03:59:15 +0700

    get user detail, CRUD data product

--- pages/administrator/produk/index.vue
+++ pages/administrator/produk/index.vue
@@ -9,32 +9,35 @@
           small
           class="text-capitalize mb-3 mr-3"
           to="/administrator/produk/add"
-        ><v-icon dark> mdi-plus </v-icon>Produk</v-btn>
+          ><v-icon dark> mdi-plus </v-icon>Produk</v-btn
+        >
         <v-btn
           color="warning"
           elevation="2"
           outlined
           small
           class="text-capitalize mb-3 mr-3"
-        ><v-icon dark> mdi-tray-arrow-down </v-icon>Download PDF</v-btn>
+          ><v-icon dark> mdi-tray-arrow-down </v-icon>Download PDF</v-btn
+        >
         <v-btn
           color="success"
           elevation="2"
           outlined
           small
           class="text-capitalize mb-3"
-        ><v-icon dark> mdi-tray-arrow-down </v-icon>Download Excel</v-btn>
+          ><v-icon dark> mdi-tray-arrow-down </v-icon>Download Excel</v-btn
+        >
       </v-col>
     </v-row>
     <v-row class="mt-0">
       <v-col cols="12" sm="8" md="5">
         <v-text-field
-            solo
-            v-model="search_code"
-            label="Cari Kode Produksi"
-            prepend-inner-icon="mdi-magnify"
-            hide-details="auto"
-          ></v-text-field>
+          solo
+          v-model="search_code"
+          label="Cari Kode Produksi"
+          prepend-inner-icon="mdi-magnify"
+          hide-details="auto"
+        ></v-text-field>
       </v-col>
     </v-row>
     <v-row>
@@ -42,105 +45,106 @@
         <v-data-table
           dense
           :headers="headers"
-          :items="products"
+          :items="getProducts"
           :search="search_code"
           class="elevation-1"
         >
           <template v-slot:[`item.id`]="{ item }">
-            <v-icon
-              class="black--text h6 pl-1"
-              @click="detailProduct(item)"
+            <v-icon class="black--text h6 pl-1" @click="detailProduct(item)"
               >mdi-information-outline
             </v-icon>
-            
+
             <v-icon
               color="success"
               @click="editProduct(item)"
               class="black--text h6 pl-1"
-            >mdi-pencil</v-icon>
+              >mdi-pencil</v-icon
+            >
+            <v-icon color="error" class="h6 pl-1" @click="deleteProduct(item)"
+              >mdi-trash-can-outline</v-icon
+            >
             <v-icon
-              color="error"
-              class="h6 pl-1"
-              @click="deleteProduct(item)"
-            >mdi-trash-can-outline</v-icon>
-            <v-icon
-              color="warning" 
-              class="h6 pl-1"
-              @click="downlaodProduct(item)"
-            >mdi-tray-arrow-down</v-icon>
-            <v-icon
-              color="success" 
+              color="warning"
               class="h6 pl-1"
               @click="downlaodProduct(item)"
-            >mdi-tray-arrow-down</v-icon>
+              >mdi-tray-arrow-down</v-icon
+            >
+            <v-icon
+              color="success"
+              class="h6 pl-1"
+              @click="downlaodProduct(item)"
+              >mdi-tray-arrow-down</v-icon
+            >
           </template>
         </v-data-table>
       </v-col>
     </v-row>
   </div>
 </template>
+
 <script>
+import { mapGetters } from "vuex";
+
 export default {
   name: "Produk",
   layout: "administrator",
-  data () {
+  async fetch({ store, route }) {
+    await store.dispatch("product/getProducts");
+  },
+  data() {
     return {
-      search_code:"",
+      search_code: "",
       headers: [
+        // {
+        //   text: "NO",
+        //   align: "start",
+        //   filterable: false,
+        //   value: "no",
+        //   class: "white--text blue",
+        // },
+        { text: "KODE PRODUKSi", value: "code", class: "white--text blue" },
+        { text: "NAMA PRODUK", value: "name", class: "white--text blue" },
         {
-          text: 'NO',
-          align: 'start',
+          text: "TANGGAL PEMBUATAN",
+          value: "created_at",
           filterable: false,
-          value: 'no',
-          class: "white--text blue" 
+          class: "white--text blue",
         },
-        { text: 'KODE PRODUKSi', value: 'code', class: "white--text blue"  },
-        { text: 'TANGGAL PEMBUATAN', value: 'date_created', filterable: false, class: "white--text blue" },
-        { text: 'TANGGAL RILIS', value: 'date_release', filterable: false, class: "white--text blue" },
-        { text: 'AKSI', value: 'id', filterable: false, class: "white--text blue" }
-      ],
-      products: [
         {
-          no: 1,
-          code: "KODEPRODUKSI1",
-          date_created: "16 September 2021",
-          date_release: "28 September 2021",
-          id: 1,
+          text: "TANGGAL RILIS",
+          value: "created_at",
+          filterable: false,
+          class: "white--text blue",
+        },
+        {
+          text: "AKSI",
+          value: "id",
+          filterable: false,
+          class: "white--text blue",
         },
       ],
-    }
+    };
   },
-  methods:{
-    fetchData(){
-      this.$store.dispatch("product/getProduct")
-      .then((response) => {
-        console.log(response)
-        if(response.code == 200){
-          this.clearForm()
-        }
-      })
-
-      // this.$store.dispatch("product/getProduct")
-      // .then((resp) => {
-      //   console.log(resp)
-      //   this.products = []
-      //   // for(let data of resp.data){
-      //   //   console.log(data, "maida")
-      //   //   this.products.push(data)
-      //   // }
-      // })
+  methods: {
+    detailProduct(id) {
+      this.$router.push({ name: "administrator-produk-detail-id", params: id });
     },
-    detailProduct(id){
-      this.$router.push({ name : "administrator-produk-detail-id", params : id})
+    editProduct(id) {
+      this.$router.push({ name: "administrator-produk-id", params: id });
     },
-    editProduct(id){
-      this.$router.push({ name : "administrator-produk-id", params : id})
+    async deleteProduct(id) {
+      await this.$api
+        .$delete(`/api/v1/product/${id.id}`)
+        .then(async (res) => {
+          if (res.code === 200) {
+            await this.$store.dispatch("product/getProducts");
+          }
+        });
     },
-    deleteProduct(id){},
-    downloadProduct(id){},
+    downloadProduct(id) {},
   },
-  mounted(){
-    this.fetchData()
-  }
+  computed: {
+    ...mapGetters("product", ["getProducts"]),
+  },
 };
 </script>
