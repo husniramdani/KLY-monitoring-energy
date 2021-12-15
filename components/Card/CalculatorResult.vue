6cfc7f37491179c9118dca8b7785b0dab83667cc
maida <maida@ainosi.co.id>
2021-12-15 11:10:17 +0700

    Calculator simple and advanced

--- components/Card/CalculatorResult.vue
+++ components/Card/CalculatorResult.vue
@@ -7,17 +7,17 @@
     <v-row v-if="customize === true" justify="space-around">
       <v-col cols="auto">
         <p class="font-weight-light mb-0">{{ title }}</p>
-        <b>{{ value }} {{ unit }}</b>
+        <b>{{ Intl.NumberFormat().format(value) }} {{ unit }}</b>
       </v-col>
       <div heigt="100%" class="border-left"></div>
       <v-col cols="auto">
         <p class="font-weight-light mb-0">Jumlah Baterai</p>
-        <b>{{value2}} Unit</b>
+        <b>{{Intl.NumberFormat().format(value2)}} Unit</b>
       </v-col>
     </v-row>
     <div v-else>
       <p class="font-weight-light mb-0">{{ title }}</p>
-      <b>{{ value }} {{ unit }}</b>
+      <b>{{ Intl.NumberFormat().format(value) }} {{ unit }}</b>
     </div>
   </v-col>
 </template>
@@ -26,8 +26,8 @@ export default {
   name: "CardCalculatorResult",
   props: {
     title: { type: String, required: true, default: "Title" },
-    value: { type: String, required: true, default: "0" },
-    value2: { type: String, required: false, default: "0" },
+    value: { type: Number, required: true, default: 0 },
+    value2: { type: Number, required: false, default: 0 },
     customize: { type: Boolean, required: false, default: false },
     unit: { type: String, required: true }
   }
